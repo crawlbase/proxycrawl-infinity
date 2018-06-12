bf0a1ab1cdfa4315f9d32a328882831e58612560
proxycrawl <info@proxycrawl.com>
2018-06-12 21:32:21 +0200

    Initial commit

---
+++ proxycrawl_stats.infinitymodule/README.md
@@ -0,0 +1,5 @@
+# ProxyCrawl Infinity Dashboard Module
+
+This module allows you to view your normal and javascript monthly counts as well as the total spent for the actual month.
+
+You should add your Normal token and your Javascript token which can be obtained from [Your Account](https://proxycrawl.com/dashboard/account) page.
---
+++ proxycrawl_stats.infinitymodule/main.js
@@ -0,0 +1,46 @@
+const fiplab = require('fiplab');
+const request = require('request');
+const util = require('util');
+
+let userArguments = fiplab.arguments;
+
+const urlFormat = 'https://proxycrawl.com/dashboard/tokens?normal=%s&javascript=%s';
+const options = {
+  url: util.format(urlFormat, userArguments.normalToken, userArguments.javascriptToken)
+};
+
+const parseJSONResponse = (json) => {
+  try {
+    const stats = json.stats;
+    let value;
+    if (userArguments.display === 'total_count') {
+      value = stats.normal_count + stats.javascript_count;
+    } else if (userArguments.display === 'total_price') {
+      value = stats.normal_price * 1 + stats.javascript_price * 1;
+    } else {
+      value = stats[userArguments.display];
+    }
+    if (userArguments.display.indexOf('_price') > -1) {
+      value = '$' + value;
+    }
+    fiplab.exit('' + value, true);
+  } catch(exception) {
+    console.error(json);
+    console.error(exception.toString());
+    fiplab.exit('Invalid response from the server.', false);
+  }
+};
+
+let callback = (error, resp, body) => {
+  if (resp.statusCode !== 200){
+    fiplab.exit('Invalid tokens.', false);
+  }
+  try {
+    let json = JSON.parse(body);
+    parseJSONResponse(json);
+  } catch(exception){
+    fiplab.exit(exception.toString(), false);
+  }
+};
+
+request(options, callback);
